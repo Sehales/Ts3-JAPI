b4448e43d051c24625c60a157aa3e34e68e21d08
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-09 18:35:17 +0200

    added CmdHostInfo.java and removed VersionInfo.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdHostInfo.java
@@ -0,0 +1,22 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.response.ArrayResponse;
+import net.sehales.ts3_japi.property.ServerInstanceProperty;
+import net.sehales.ts3_japi.wrapper.MapWrapper;
+import net.sehales.ts3_japi.wrapper.Wrapper;
+
+public class CmdHostInfo extends Command {
+
+    public CmdHostInfo() {
+        super("hostinfo");
+    }
+
+    /**
+     * Take a look at {@link ServerInstanceProperty} for possible properties you can get from that {@link MapWrapper}
+     */
+    @Override
+    public Wrapper getResponseWrapper() {
+        return new MapWrapper(((ArrayResponse) response).getResponseData().get(0));
+    }
+
+}
--- TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdVersion.java
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdVersion.java
@@ -1,7 +1,8 @@
 package net.sehales.ts3_japi.command;
 
 import net.sehales.ts3_japi.command.response.ArrayResponse;
-import net.sehales.ts3_japi.wrapper.VersionInfo;
+import net.sehales.ts3_japi.property.VersionInfoProperty;
+import net.sehales.ts3_japi.wrapper.MapWrapper;
 
 public class CmdVersion extends Command {
 
@@ -9,9 +10,12 @@ public class CmdVersion extends Command {
         super("version");
     }
 
+    /**
+     * Take a look at {@link VersionInfoProperty} for possible properties you can get from that {@link MapWrapper}
+     */
     @Override
-    public VersionInfo getResponseWrapper() {
-        return new VersionInfo(((ArrayResponse) response).getResponseData().get(0));
+    public MapWrapper getResponseWrapper() {
+        return new MapWrapper(((ArrayResponse) response).getResponseData().get(0));
     }
 
 }
