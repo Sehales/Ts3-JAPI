976cbca5915c01e3b84a0eac01ebe1928cb92ace
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-09 23:19:09 +0200

    added CmdServerEdit.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdServerEdit.java
@@ -0,0 +1,39 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.property.ServerInstanceProperty;
+import net.sehales.ts3_japi.property.VirtualServerProperty;
+
+public class CmdServerEdit extends Command {
+
+    public CmdServerEdit() {
+        super("serveredit");
+    }
+
+    /**
+     * @see #addParameter(ServerInstanceProperty, String)
+     */
+    public CmdServerEdit(VirtualServerProperty property, String value) {
+        this();
+        addParameter(property, value);
+    }
+
+    public CmdServerEdit addParameter(String key, String value) {
+        add(new KeyValueParameter(key, value));
+        return this;
+    }
+
+    /**
+     * All unchangeable properties passed into it will be ignored
+     * 
+     * @param property
+     * @param value
+     * @return
+     */
+    public CmdServerEdit addParameter(VirtualServerProperty property, String value) {
+        if (property.isChangeable()) {
+            return addParameter(property.getName(), value);
+        }
+        return this;
+    }
+}
