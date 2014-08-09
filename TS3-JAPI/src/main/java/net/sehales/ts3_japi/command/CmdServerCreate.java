1ee727a3acd84cbe4b36f7ac38623c7a11ec6ccf
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-09 23:02:58 +0200

    added CmdServerCreate.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdServerCreate.java
@@ -0,0 +1,31 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.property.VirtualServerProperty;
+
+public class CmdServerCreate extends Command {
+
+    public CmdServerCreate() {
+        super("servercreate");
+    }
+
+    public CmdServerCreate addParameter(String key, String value) {
+        add(new KeyValueParameter(key, value));
+        return this;
+    }
+
+    /**
+     * All unchangeable properties passed into it will be ignored
+     * 
+     * @param property
+     * @param value
+     * @return
+     */
+    public CmdServerCreate addParameter(VirtualServerProperty property, String value) {
+        if (property.isChangeable()) {
+            return addParameter(property.getName(), value);
+        }
+        return this;
+    }
+
+}
