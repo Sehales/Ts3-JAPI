2861a85e768402b7e8fed60e00c202eddfde4118
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-11 22:50:45 +0200

    added CmdServerGroupAutoAddPerm.java and DefaultAutoPermGroupType.java (with the default auto update values for the default server groups)

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdServerGroupAutoAddPerm.java
@@ -0,0 +1,68 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.QueryUtils;
+import net.sehales.ts3_japi.command.parameter.ArrayParameter;
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.command.parameter.ListParameter;
+import net.sehales.ts3_japi.property.DefaultAutoPermGroupType;
+
+public class CmdServerGroupAutoAddPerm extends Command {
+
+    private ArrayParameter array = new ArrayParameter();
+
+    public CmdServerGroupAutoAddPerm(DefaultAutoPermGroupType groupType) {
+        this(groupType.getIndex());
+    }
+
+    public CmdServerGroupAutoAddPerm(DefaultAutoPermGroupType groupType, int permId, int permValue, boolean permNegated, boolean permSkip) {
+        this(groupType);
+        addPerm(permId, permValue, permNegated, permSkip);
+    }
+
+    public CmdServerGroupAutoAddPerm(DefaultAutoPermGroupType groupType, String permsId, int permValue, boolean permNegated, boolean permSkip) {
+        this(groupType);
+        addPerm(permsId, permValue, permNegated, permSkip);
+    }
+
+    public CmdServerGroupAutoAddPerm(int groupType) {
+        super("servergroupautoaddperm");
+        add(new KeyValueParameter("sgtype", groupType));
+    }
+
+    public CmdServerGroupAutoAddPerm(int groupType, int permId, int permValue, boolean permNegated, boolean permSkip) {
+        this(groupType);
+        addPerm(permId, permValue, permNegated, permSkip);
+    }
+
+    public CmdServerGroupAutoAddPerm(int groupType, String permsId, int permValue, boolean permNegated, boolean permSkip) {
+        this(groupType);
+        addPerm(permsId, permValue, permNegated, permSkip);
+    }
+
+    public CmdServerGroupAutoAddPerm addPerm(int permId, int permValue, boolean permNegated, boolean permSkip) {
+        array.add(new ListParameter(
+                        new KeyValueParameter("permid", permId),
+                        new KeyValueParameter("permvalue", permValue),
+                        new KeyValueParameter("permnegated", QueryUtils.toInt(permNegated)),
+                        new KeyValueParameter("permskip", QueryUtils.toInt(permSkip))
+                        ));
+        return this;
+    }
+
+    public CmdServerGroupAutoAddPerm addPerm(String permsId, int permValue, boolean permNegated, boolean permSkip) {
+        array.add(new ListParameter(
+                        new KeyValueParameter("permsid", permsId),
+                        new KeyValueParameter("permvalue", permValue),
+                        new KeyValueParameter("permnegated", QueryUtils.toInt(permNegated)),
+                        new KeyValueParameter("permskip", QueryUtils.toInt(permSkip))
+                        ));
+        return this;
+    }
+
+    @Override
+    public String buildString() {
+        add(array);
+        return super.buildString();
+    }
+
+}
---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/property/DefaultAutoPermGroupType.java
@@ -0,0 +1,23 @@
+package net.sehales.ts3_japi.property;
+
+public enum DefaultAutoPermGroupType {
+    CHANNEL_GUEST(10),
+    SERVER_GUEST(15),
+    QUERY_GUEST(20),
+    CHANNEL_VOICE(25),
+    SERVER_NORMAL(30),
+    CHANNEL_OPERATOR(35),
+    CHANNEL_ADMIN(40),
+    SERVER_ADMIN(45),
+    QUERY_ADMIN(50);
+
+    private int index;
+
+    DefaultAutoPermGroupType(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
