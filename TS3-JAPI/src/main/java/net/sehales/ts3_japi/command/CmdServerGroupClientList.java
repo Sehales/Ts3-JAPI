175e22630463752b1f67c74fef66e267c9b5fe9c
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-11 16:38:35 +0200

    added CmdServerGroupClientList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdServerGroupClientList.java
@@ -0,0 +1,17 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdServerGroupClientList extends Command {
+
+    public CmdServerGroupClientList(int serverGroupId) {
+        super("servergroupclientlist");
+        add(new KeyValueParameter("sgid", serverGroupId));
+    }
+
+    public CmdServerGroupClientList names() {
+        add(new OptionParameter("names"));
+        return this;
+    }
+}
