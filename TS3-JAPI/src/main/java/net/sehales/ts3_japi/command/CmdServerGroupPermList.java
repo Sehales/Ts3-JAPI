cfc6db0aec33d7a37b6968f73d487f47d7c222c2
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-11 14:01:53 +0200

    added CmdServerGroupPermList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdServerGroupPermList.java
@@ -0,0 +1,21 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdServerGroupPermList extends Command {
+
+    public CmdServerGroupPermList(int serverGroupId) {
+        super("servergrouppermlist");
+        add(new KeyValueParameter("sgid", serverGroupId));
+    }
+
+    /**
+     * @return permission names instead of internal ids
+     */
+    public CmdServerGroupPermList permsId() {
+        add(new OptionParameter("permsid"));
+        return this;
+    }
+
+}
