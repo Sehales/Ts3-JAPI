17ba672f615401b09275b9999b223efbbea61d18
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-11 23:11:23 +0200

    added CmdServerSnapshotCreate.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdServerSnapshotCreate.java
@@ -0,0 +1,9 @@
+package net.sehales.ts3_japi.command;
+
+public class CmdServerSnapshotCreate extends Command {
+
+    public CmdServerSnapshotCreate() {
+        super("serversnapshotcreate");
+    }
+
+}
