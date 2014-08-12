b21014174db12dfb1472d087093a1701db2bf7ea
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-12 18:35:19 +0200

    added CmdGM.java and forgot to commit LogLevel.java and TextMessageTargetMode.java

--- TS3-JAPI/src/main/java/net/sehales/ts3_japi/property/LogLevel.java
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/property/LogLevel.java
@@ -1,13 +1,14 @@
 package net.sehales.ts3_japi.property;
 
-public enum TextMessageTargetMode {
-    CLIENT(1),
-    CHANNEL(2),
-    SERVER(3);
+public enum LogLevel {
+    LogLevel_ERROR(1),
+    LogLevel_WARNING(2),
+    LogLevel_DEBUG(3),
+    LogLevel_INFO(4);
 
     private int index;
 
-    TextMessageTargetMode(int index) {
+    LogLevel(int index) {
         this.index = index;
     }
 
