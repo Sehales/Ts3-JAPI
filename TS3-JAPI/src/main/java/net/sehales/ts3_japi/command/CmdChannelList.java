a0f384c488a48dd2e8238e6603821dd4c77d9fd9
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-12 18:37:54 +0200

    added CmdChannelList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdChannelList.java
@@ -0,0 +1,36 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdChannelList extends Command {
+
+    public CmdChannelList() {
+        super("channellist");
+    }
+
+    public CmdChannelList flags() {
+        add(new OptionParameter("flags"));
+        return this;
+    }
+
+    public CmdChannelList icon() {
+        add(new OptionParameter("icon"));
+        return this;
+    }
+
+    public CmdChannelList limits() {
+        add(new OptionParameter("limits"));
+        return this;
+    }
+
+    public CmdChannelList topic() {
+        add(new OptionParameter("topic"));
+        return this;
+    }
+
+    public CmdChannelList voice() {
+        add(new OptionParameter("voice"));
+        return this;
+    }
+
+}
