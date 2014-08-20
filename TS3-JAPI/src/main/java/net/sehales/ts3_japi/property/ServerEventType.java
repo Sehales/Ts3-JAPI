9ed17a28ae896ea4c6bc391d773adaad3894329f
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-20 13:49:18 +0200

    added undocumented "TOKENUSED" event to ServerEventType.java

--- TS3-JAPI/src/main/java/net/sehales/ts3_japi/property/ServerEventType.java
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/property/ServerEventType.java
@@ -1,7 +1,7 @@
 package net.sehales.ts3_japi.property;
 
 public enum ServerEventType {
-    SERVER, CHANNEL, TEXTSERVER, TEXTCHANNEL, TEXTPRIVATE;
+    SERVER, CHANNEL, TEXTSERVER, TEXTCHANNEL, TEXTPRIVATE, TOKENUSED;
 
     public String getName() {
         return name().toLowerCase();
