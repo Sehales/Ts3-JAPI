0387755f237c9bfbbbf04c3c406d1e90a03b6fc1
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-24 19:57:35 +0200

    added CmdChannelGroupClientList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdChannelGroupClientList.java
@@ -0,0 +1,25 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdChannelGroupClientList extends Command {
+
+    public CmdChannelGroupClientList() {
+        super("channelgroupclientlist");
+    }
+
+    public CmdChannelGroupClientList channelGroupId(int channelGroupId) {
+        add(new KeyValueParameter("cgid", channelGroupId));
+        return this;
+    }
+
+    public CmdChannelGroupClientList channelId(int channelId) {
+        add(new KeyValueParameter("cid", channelId));
+        return this;
+    }
+
+    public CmdChannelGroupClientList clientDbId(int clientDbId) {
+        add(new KeyValueParameter("cldbid", clientDbId));
+        return this;
+    }
+}
