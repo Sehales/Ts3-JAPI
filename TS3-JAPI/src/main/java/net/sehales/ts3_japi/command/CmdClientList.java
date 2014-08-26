c48dcbcdecf2b47d58ba414cc252ef38c8167bd9
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-26 16:06:16 +0200

    added CmdClientList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientList.java
@@ -0,0 +1,51 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdClientList extends Command {
+
+    public CmdClientList() {
+        super("clientlist");
+    }
+
+    public CmdClientList away() {
+        add(new OptionParameter("away"));
+        return this;
+    }
+
+    public CmdClientList country() {
+        add(new OptionParameter("country"));
+        return this;
+    }
+
+    public CmdClientList groups() {
+        add(new OptionParameter("groups"));
+        return this;
+    }
+
+    public CmdClientList icon() {
+        add(new OptionParameter("icon"));
+        return this;
+    }
+
+    public CmdClientList info() {
+        add(new OptionParameter("info"));
+        return this;
+    }
+
+    public CmdClientList times() {
+        add(new OptionParameter("times"));
+        return this;
+    }
+
+    public CmdClientList uid() {
+        add(new OptionParameter("uid"));
+        return this;
+    }
+
+    public CmdClientList voice() {
+        add(new OptionParameter("voice"));
+        return this;
+    }
+
+}
