b0a7a72c96657fe5134d723bafb63920034a14f2
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-27 14:47:36 +0200

    added CmdClientDbList.java

--- TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientDbList.java
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientDbList.java
@@ -19,6 +19,22 @@ public class CmdClientDbList extends Command {
         return this;
     }
 
+    /**
+     * alias for duration because of that not well chosen parameter name
+     * 
+     * @param limit
+     * @return
+     */
+    public CmdClientDbList limit(long limit) {
+        return duration(limit);
+    }
+
+    /**
+     * starts at 0
+     * 
+     * @param offset
+     * @return
+     */
     public CmdClientDbList start(long offset) {
         add(new KeyValueParameter("start", "" + offset));
         return this;
