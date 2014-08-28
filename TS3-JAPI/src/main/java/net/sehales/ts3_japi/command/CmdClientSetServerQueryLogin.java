567fabc46e4cded5f131685bf4ae0b0043fa37ae
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-28 17:49:03 +0200

    added ClientSetServerQueryLogin.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientSetServerQueryLogin.java
@@ -0,0 +1,12 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdClientSetServerQueryLogin extends Command {
+
+    public CmdClientSetServerQueryLogin(String loginName) {
+        super("clientsetserverquerylogin");
+        add(new KeyValueParameter("client_login_name", loginName));
+    }
+
+}
