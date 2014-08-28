648d76ec9c22d05d3fae249ca3475c22d3035340
Jan 'Sehales' Philipp <jan@sehales.net>
2014-08-28 17:30:17 +0200

    added CmdClientDbFind.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientDbFind.java
@@ -0,0 +1,18 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdClientDbFind extends Command {
+
+    public CmdClientDbFind(String pattern) {
+        super("clientdbfind");
+        add(new KeyValueParameter("pattern", pattern));
+    }
+
+    public CmdClientDbFind uid() {
+        add(new OptionParameter("uid"));
+        return this;
+    }
+
+}
