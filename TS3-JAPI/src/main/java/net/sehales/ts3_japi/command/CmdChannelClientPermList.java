5204312180721b5a5e4cd94c24f1a991ca6822fa
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-02 19:59:35 +0200

    added CmdChannelClientPermList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdChannelClientPermList.java
@@ -0,0 +1,18 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdChannelClientPermList extends Command {
+
+    public CmdChannelClientPermList(int channelId, int clientDbId) {
+        super("channelclientpermlist");
+        add(new KeyValueParameter("cid", channelId));
+        add(new KeyValueParameter("cldbid", clientDbId));
+    }
+
+    public CmdChannelClientPermList permsid() {
+        add(new OptionParameter("permsid"));
+        return this;
+    }
+}
