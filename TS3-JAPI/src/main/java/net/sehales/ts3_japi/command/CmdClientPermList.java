28046f89aad3a5bb40c8a9a4f6ec7f65ffcf5461
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-02 19:50:10 +0200

    added CmdClientPermList.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientPermList.java
@@ -0,0 +1,17 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+import net.sehales.ts3_japi.command.parameter.OptionParameter;
+
+public class CmdClientPermList extends Command {
+
+    public CmdClientPermList(int clientDbId) {
+        super("clientpermlist");
+        add(new KeyValueParameter("cldbid", clientDbId));
+    }
+
+    public CmdClientPermList permsid() {
+        add(new OptionParameter("permsid"));
+        return this;
+    }
+}
