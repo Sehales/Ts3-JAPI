76aa1b010350f99743d56b2115a2c739d242e9b8
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-02 20:03:27 +0200

    added permskip values to CmdClientAddPerm

--- TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientAddPerm.java
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdClientAddPerm.java
@@ -1,5 +1,6 @@
 package net.sehales.ts3_japi.command;
 
+import net.sehales.ts3_japi.QueryUtils;
 import net.sehales.ts3_japi.command.parameter.ArrayParameter;
 import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
 import net.sehales.ts3_japi.command.parameter.ListParameter;
@@ -12,28 +13,30 @@ public class CmdClientAddPerm extends Command {
         add(new KeyValueParameter("cldbid", clientDbId));
     }
 
-    public CmdClientAddPerm(int clientDbId, int permId, int permValue) {
+    public CmdClientAddPerm(int clientDbId, int permId, int permValue, boolean permSkip) {
         this(clientDbId);
-        addPerm(permId, permValue);
+        addPerm(permId, permValue, permSkip);
     }
 
-    public CmdClientAddPerm(int clientDbId, String permsId, int permValue) {
+    public CmdClientAddPerm(int clientDbId, String permsId, int permValue, boolean permSkip) {
         this(clientDbId);
-        addPerm(permsId, permValue);
+        addPerm(permsId, permValue, permSkip);
     }
 
-    public CmdClientAddPerm addPerm(int permId, int permValue) {
+    public CmdClientAddPerm addPerm(int permId, int permValue, boolean permSkip) {
         array.add(new ListParameter(
                         new KeyValueParameter("permid", permId),
-                        new KeyValueParameter("permvalue", permValue)
+                        new KeyValueParameter("permvalue", permValue),
+                        new KeyValueParameter("permskip", QueryUtils.toInt(permSkip))
                         ));
         return this;
     }
 
-    public CmdClientAddPerm addPerm(String permsId, int permValue) {
+    public CmdClientAddPerm addPerm(String permsId, int permValue, boolean permSkip) {
         array.add(new ListParameter(
                         new KeyValueParameter("permsid", permsId),
-                        new KeyValueParameter("permvalue", permValue)
+                        new KeyValueParameter("permvalue", permValue),
+                        new KeyValueParameter("permskip", QueryUtils.toInt(permSkip))
                         ));
         return this;
     }
