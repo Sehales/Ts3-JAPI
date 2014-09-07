536174fd5545c1604b8d7a49ceb8ae3196f122cc
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-07 17:05:08 +0200

    added CmdPermGet.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdPermGet.java
@@ -0,0 +1,21 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdPermGet extends Command {
+
+    private CmdPermGet() {
+        super("permget");
+    }
+
+    public CmdPermGet(int permId) {
+        this();
+        add(new KeyValueParameter("permid", permId));
+    }
+
+    public CmdPermGet(String permsId) {
+        this();
+        add(new KeyValueParameter("permsid", permsId));
+    }
+
+}
