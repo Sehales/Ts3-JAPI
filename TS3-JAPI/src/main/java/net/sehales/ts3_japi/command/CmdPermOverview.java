db76e603abdbf804dd0ab7358bb612574e5cd3ad
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-07 16:53:02 +0200

    added CmdPermOverview.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdPermOverview.java
@@ -0,0 +1,23 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdPermOverview extends Command {
+
+    private CmdPermOverview(int channelId, int clientDbId) {
+        super("permoverview");
+        add(new KeyValueParameter("cid", channelId));
+        add(new KeyValueParameter("cldbid", clientDbId));
+    }
+
+    public CmdPermOverview(int channelId, int clientDbId, int permId) {
+        this(channelId, clientDbId);
+        add(new KeyValueParameter("permid", permId));
+    }
+
+    public CmdPermOverview(int channelId, int clientDbId, String permsId) {
+        this(channelId, clientDbId);
+        add(new KeyValueParameter("permsid", permsId));
+    }
+
+}
