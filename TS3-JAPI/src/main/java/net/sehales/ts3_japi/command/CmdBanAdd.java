dd4963814bcd94acf4293e60cc48a9605d705fab
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-19 16:43:36 +0200

    added CmdBanAdd.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdBanAdd.java
@@ -0,0 +1,36 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdBanAdd extends Command {
+
+    public CmdBanAdd() {
+        super("banadd");
+    }
+
+    public CmdBanAdd withIp(String regexp) {
+        add(new KeyValueParameter("ip", regexp));
+        return this;
+    }
+
+    public CmdBanAdd withName(String regexp) {
+        add(new KeyValueParameter("name", regexp));
+        return this;
+    }
+
+    public CmdBanAdd withReason(String message) {
+        add(new KeyValueParameter("banreason", message));
+        return this;
+    }
+
+    public CmdBanAdd withTime(long timeInSeconds) {
+        add(new KeyValueParameter("time", timeInSeconds));
+        return this;
+    }
+
+    public CmdBanAdd withUid(String clientUid) {
+        add(new KeyValueParameter("uid", clientUid));
+        return this;
+    }
+
+}
