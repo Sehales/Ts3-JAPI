c9c59a607e1aebc8a295fa35c9c05b7091ca9bee
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-19 16:31:08 +0200

    added CmdBanClient.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdBanClient.java
@@ -0,0 +1,27 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdBanClient extends Command {
+
+    public CmdBanClient(int clientId) {
+        super("banclient");
+        add(new KeyValueParameter("clid", clientId));
+    }
+
+    public CmdBanClient(int clientId, long timeInSeconds) {
+        this(clientId);
+        add(new KeyValueParameter("time", "" + timeInSeconds));
+    }
+
+    public CmdBanClient(int clientId, long timeInSeconds, String banReason) {
+        this(clientId, timeInSeconds);
+        add(new KeyValueParameter("banreason", banReason));
+    }
+
+    public CmdBanClient(int clientId, String banReason) {
+        this(clientId);
+        add(new KeyValueParameter("banreason", banReason));
+    }
+
+}
