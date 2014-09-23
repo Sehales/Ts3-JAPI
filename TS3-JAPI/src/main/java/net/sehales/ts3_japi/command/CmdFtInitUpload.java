3e252f0b40c579567286eb387a0c0bc4ff6c106c
Jan 'Sehales' Philipp <jan@sehales.net>
2014-09-23 15:09:13 +0200

    added CmdFtInitUpload.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdFtInitUpload.java
@@ -0,0 +1,19 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.QueryUtils;
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdFtInitUpload extends Command {
+
+    public CmdFtInitUpload(int clientFileTransferId, String fileName, int channelId, String channelPassword, long size, boolean overwrite, boolean resume) {
+        super("ftinitupload");
+        add(new KeyValueParameter("clientftfid", clientFileTransferId));
+        add(new KeyValueParameter("name", fileName));
+        add(new KeyValueParameter("cid", channelId));
+        add(new KeyValueParameter("cpw", channelPassword));
+        add(new KeyValueParameter("size", size));
+        add(new KeyValueParameter("overwrite", QueryUtils.toInt(overwrite)));
+        add(new KeyValueParameter("resume", QueryUtils.toInt(resume)));
+    }
+
+}
