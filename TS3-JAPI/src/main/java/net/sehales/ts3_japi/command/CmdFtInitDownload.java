149a9d93c33d16c46f00b2135cc648a86935b21a
Jan 'Sehales' Philipp <jan@sehales.net>
2014-10-30 20:03:27 +0100

    added CmdFtInitDownload.java

---
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/command/CmdFtInitDownload.java
@@ -0,0 +1,16 @@
+package net.sehales.ts3_japi.command;
+
+import net.sehales.ts3_japi.command.parameter.KeyValueParameter;
+
+public class CmdFtInitDownload extends Command {
+
+    public CmdFtInitDownload(int clientFileTransferId, String filePath, int channelId, String channelPassword, long seekPos) {
+        super("ftinitdownload");
+        add(new KeyValueParameter("clientftfid", clientFileTransferId));
+        add(new KeyValueParameter("name", filePath));
+        add(new KeyValueParameter("cid", channelId));
+        add(new KeyValueParameter("cpw", channelPassword));
+        add(new KeyValueParameter("seekpos", seekPos));
+    }
+
+}
