333bb4d4d96faf149dad823d467dfae40913454c
Jan 'Sehales' Philipp <jan@sehales.net>
2015-03-03 01:37:25 +0100

    added serverList() to ServerQueryAPI added appropriate wrapper class added response utils

--- TS3-JAPI/src/main/java/net/sehales/ts3_japi/ServerQueryAPI.java
+++ TS3-JAPI/src/main/java/net/sehales/ts3_japi/ServerQueryAPI.java
@@ -1,14 +1,56 @@
 package net.sehales.ts3_japi;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import net.sehales.ts3_japi.command.CmdServerList;
+import net.sehales.ts3_japi.command.Command;
+import net.sehales.ts3_japi.util.ResponseUtil;
+import net.sehales.ts3_japi.wrapper.Server;
+
 public class ServerQueryAPI {
-    @SuppressWarnings("unused")
-    // will get implemented/used later
-    private ServerQuery query;
+    private ServerQuery             query;
+    private ObjectProperty<Command> lastCommand = new SimpleObjectProperty<>();
 
     @SuppressWarnings("unused")
     private ServerQueryAPI() {}
 
     ServerQueryAPI(ServerQuery query) {
         this.query = query;
+    }
+
+    public Command getLastCommand() {
+        return lastCommandProperty().getValue();
+    }
+
+    public ObjectProperty<Command> lastCommandProperty() {
+        return lastCommand;
+    }
+
+    private void send(Command cmd) {
+        query.send(cmd);
+        setLastCommand(cmd);
+    }
+
+    /**
+     * Get a list of all servers present in the database.
+     * 
+     * This also contains servers which are running in another instance, but are stored in the same database.
+     * 
+     * @return The server list.
+     */
+    public ObservableList<Server> serverList() {
+        CmdServerList cmd = new CmdServerList().all().uid();
+        send(cmd);
+        if (!cmd.hasResponse()) {
+            return FXCollections.observableArrayList();
+        }
+        return ResponseUtil.convertServerListResponse(cmd.getResponseWrapper());
+    }
+
+    private void setLastCommand(final Command lastCommand) {
+        lastCommandProperty().setValue(lastCommand);
     }
 }
